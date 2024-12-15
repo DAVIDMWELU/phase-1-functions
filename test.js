24f1bbaca1806b829e6779062196de52232d4410
DAVIDMWELU <davidmwelu@gmail.com>
2024-12-15 16:30:35 +0300

    Initial commit with passing test

---
+++ test.js
@@ -0,0 +1,8 @@
+// test/example.test.js
+const assert = require('assert');
+
+describe('Simple Test', function() {
+  it('should add 1 + 1 to equal 2', function() {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
